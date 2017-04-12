e522a2fddbe6911a60b0a46f7b5e14e5c3ea7b03
Max <regforblank@gmail.com>
2017-04-12 09:34:19 +0300

    .

--- resources/create_sea.py
+++ resources/create_sea.py
@@ -12,10 +12,16 @@ os.system('clear')
 filename = 'sea.csv'
 with open(filename, newline='') as f:
     reader = csv.reader(f)
+    
     try:
-        for row in reader:
-            print(row)
-    except csv.Error as e:
-        sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+        # занесение csv в [ список\list ]
+        sea = list(reader)
+        
 
- 
+            
+    except csv.Error as e:
+        sys.exit('file {}, line {}: {}' \
+        .format(filename, reader.line_num, e))
+
+
+print(sea)
