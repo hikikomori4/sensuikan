9c0d249ce66c4a9841a43300e4964b80a9ce2203
Max <regforblank@gmail.com>
2017-04-12 09:35:48 +0300

    описание ДПЛ начал...

--- resources/uboat.py
+++ resources/uboat.py
@@ -2,7 +2,66 @@
 # coding: utf-8
 
 
+uboat
 
+class Uboat(object):
+    def measurements(self):
+        # Главные размерения субмарины
+        length =
+        width =
+        height =
+        tonnage =
+        tonnage_sub =
+        draft =   # осадка
+
+
+    def complement(self):
+        # Комплектация команды
+        pass
+
+    def ttx(self):
+        # Тактико-технические характеристики
+        
+        depth_safe =
+        depth_work =
+        depth_max =
+        depth_dsg =
+        
+class Compartment(object, Uboat):
+    # Отсеки от носа к корме
+    pass
+    
+
+    def part1(self):
+        # Отсек
+        pass
+         
+    def part2(self):
+        # Отсек
+        pass
+         
+    def part3(self):
+        # Отсек
+        pass
+         
+    def part4(self):
+        # Отсек
+        pass
+         
+    def part5(self):
+        # Отсек
+        pass
+         
+    def part6(self):
+        # Отсек
+        pass
+         
+    def part7(self):
+        # Отсек
+        pass
+         
+        
+        
 
 
 if __name__ == '__main__':
