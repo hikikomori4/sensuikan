04ea98ff4ce6df71adb7902766375e08f002a0a4
Max <regforblank@gmail.com>
2017-04-13 22:55:43 +0300

    словари проблема

--- main.py
+++ main.py
@@ -5,8 +5,35 @@
 
 # from resources import sea, uboat
 
+# print(uboat.compartments)
+
+'''
+people = {'Alice': {'phone': '2341', 'addr': 'Foo drive 23' },
+          'Beth':  {'phone': '9102', 'addr': 'Bar street 42'}}
+name = 'Alice'          
+key = 'phone'
+if name in people: 
+  print("%s phone is %s" % (name, people[name][key]))
+'''
+
+ 
 
 
 
 
+tanks = {
+    {'differential_1': 
+        {
+          'name': 'Носовая дифферентная цистерна',
+          'Capacity': 1000,
+          'state': 'цистерна в порядке' 
+        }},
+    {'differential_2': 
+        {
+          'name': 'Кормовая дифферентная цистерна',
+          'Capacity': 1000,
+          'state': 'цистерна в порядке' 
+        }}
+    }
 
+print(tanks['differential_2']['name'])
